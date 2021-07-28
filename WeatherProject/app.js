55c3923c99075ed2a57c0d976e7fc800a9b014dd
jsandoval21 <juansndvl89@gmail.com>
2021-07-28 15:55:44 -0700

    working on the weatherProject

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -3,18 +3,36 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
+
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=chico,ca,usa&appid=8035b3d6e733ac5b16d74e4c8a8a8fda&units=imperial";
+  res.sendFile(__dirname + "/index.html")
+
+});
+
+app.listen(port, function() {
+  console.log("Server is running on port 3000.");
+});
+
+app.post("/", function(req, res){
+
+  const query = req.body.cityName;
+  const apiKey = "8035b3d6e733ac5b16d74e4c8a8a8fda";
+  const units = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + ",ca,usa&appid=" + apiKey + "&units=" + units;
+
   https.get(url, function(response){
     console.log(response.statusCode);
 
     response.on("data", function(data){
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const icon = weatherData.weather[1].icon;
+      const icon = weatherData.weather[0].icon;
       const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       const weatherDesc = weatherData.weather[0].description;
 
@@ -22,15 +40,10 @@ app.get("/", function(req, res){
       console.log(weatherDesc);
       console.log(icon);
 
-      res.write("<h1>Current temperature in Chico is: " + temp + " fahrenheit.</h1>");
-      res.write("<p>The weather in chico is: " + weatherDesc + "</p>");
+      res.write("<h1>Current temperature in " + query + " is: " + temp + " fahrenheit.</h1>");
+      res.write("<p>The weather in " + query + " is: " + weatherDesc + "</p>");
       res.write("<img src=" + imageURL + ">");
       res.end();
     });
   });
-
-});
-
-app.listen(port, function() {
-  console.log("Server is running on port 3000.");
-});
+})
